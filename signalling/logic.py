d27685e8f5310d9d0e4c9c595637e0f6bc95845c
Cielak <4882202+cielak@users.noreply.github.com>
2019-10-17 08:30:12 +0200

    Specified more implementation details

--- signalling/logic.py
+++ signalling/logic.py
@@ -11,17 +11,29 @@ class CollisionElement:
     def arrival_time(self):
         raise NotImplementedError
 
-
-class UnorderedPair:
-    def inverted(self):
+    @property
+    def evacuation_yellow_time(self):
         raise NotImplementedError
 
 
-class CollisionPair(CollisionElement, UnorderedPair):
-    pass
+class CollisionPair(ReversiblePair):
+    a = CollisionElement
+    b = CollisionElement
+
+    @property
+    def elements(self):
+        return self.a, self.b
+
+    @property
+    def inverted_elements(self):
+        return self.b, self.a
 
 
 class IntergreenPair:
+    evacuating = IntergreenElement
+    arriving = IntergreenElement
+    intergreen_time = int
+
     def __init__(self, evacuating, arriving, intergreen_time):
         raise NotImplementedError
 
@@ -37,12 +49,15 @@ def all_intergreen_pairs(
     collision_pairs: t.Iterable[CollisionPair]
 ) -> t.Iterable[IntergreenPair]:
     for collision_pair in collision_pairs:
-        for evacuating, arriving in [collision_pair, collision_pair.inverted()]:
+        for evacuating, arriving in [
+            collision_pair.elements,
+            collision_pair.inverted_elements,
+        ]:
             yield IntergreenPair(
                 evacuating,
                 arriving,
                 intergreen_time(
-                    evacuating.yellow_time,
+                    evacuating.evacuation_yellow_time,
                     evacuating.time_evacuation,
                     arriving.time_arrival,
                 ),
