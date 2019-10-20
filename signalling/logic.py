da520f68ed1afed8c0d3d3090c6fb17a56f9d818
Cielak <4882202+cielak@users.noreply.github.com>
2019-10-20 21:45:58 +0200

    Refactor to apply more top-down view of the issue

--- signalling/logic.py
+++ signalling/logic.py
@@ -1,12 +1,19 @@
 import typing as t
+from itertools import product
 from math import ceil
 
 
-class SignallingGroup:
-    name = str
+class TrafficStream:
+    evacuation_velocity = int
+    arrival_velocity = int
+    evacuation_yellow_time = int
+    vehicle_length = int
+
+    def intersects(self, other_stream):
+        raise NotImplementedError
 
 
-class CollisionElement:
+class CollisionPoint:
     @property
     def evacuation_time(self) -> float:
         raise NotImplementedError
@@ -20,32 +27,32 @@ class CollisionElement:
         raise NotImplementedError
 
 
-class IntergreenElement:
-    pass
+class SignallingGroup:
+    name = str
 
 
-class CollisionPair:
-    a = CollisionElement
-    b = CollisionElement
-
-    @property
-    def elements(self) -> t.Tuple[t.Type[CollisionElement], t.Type[CollisionElement]]:
-        return self.a, self.b
+class SignallingPhase:
+    green_groups = set
+    red_groups = set
 
     @property
-    def inverted_elements(
-        self
-    ) -> t.Tuple[t.Type[CollisionElement], t.Type[CollisionElement]]:
-        return self.b, self.a
-
-
-class IntergreenPair:
-    evacuating = IntergreenElement
-    arriving = IntergreenElement
-    intergreen_time = int
-
-    def __init__(self, evacuating, arriving, intergreen_time):
+    def saturation_level(self):
         raise NotImplementedError
+
+
+class PhaseTransition:
+    in_phase = SignallingPhase
+    out_phase = SignallingPhase
+
+    @property
+    def minimal_duration(self):
+        return interphase_min_time(self.in_phase, self.out_phase)
+
+
+class SignallingProgram:
+    groups = t.Set[SignallingGroup]
+    phases = t.Set[SignallingPhase]
+    phase_transitions = t.Set[PhaseTransition]
 
 
 def intergreen_time(
@@ -56,13 +63,13 @@ def intergreen_time(
 
 
 def stream_intergreen_time(evacuating_stream, arriving_stream):
-    collision_point = evacuating_stream.intercests(arriving_stream)
+    collision_point = evacuating_stream.intersects(arriving_stream)
     if not collision_point:
         return None
-    return intergree_time(
+    return intergreen_time(
         evacuating_stream.evacuating_yellow_time,
-        collision_point.evacuating_time,
-        collision_point.arriving_time,
+        collision_point.evacuation_time,
+        collision_point.arrival_time,
     )
 
 
@@ -70,27 +77,18 @@ def group_intergreen_time(evacuating_group, arriving_group):
     if evacuating_group == arriving_group:
         return None
     else:
-        stream_intergreen_times = []
-        for evacuating_stream in evacuating_group.streams:
-            for arriving_stream in arriving_group.streams:
-                stream_intergreen_times.append(
-                    stream_intergreen_time(evacuating_stream, arriving_stream)
-                )
-        return max(stream_intergreen_times)
-
-
-def intergreen_times(signalling_groups):
-    for group in signalling_groups:
-        for other_group in signalling_groups:
-            yield IntergreenPair(
-                group, other_group, group_intergreen_time(group, other_group)
+        return max(
+            stream_intergreen_time(evacuating_stream, arriving_stream)
+            for evacuating_stream, arriving_stream in product(
+                evacuating_group.streams, arriving_group.streams
             )
+        )
 
 
 def interphase_min_time(in_phase, out_phase):
-    evacuating_groups = intersection(in_phase.green_groups, out_phase.red_groups)
-    arriving_groups = intersection(in_phase.red_groupa, out_phase.green_groups)
+    evacuating_groups = in_phase.green_groups.intersection(out_phase.red_groups)
+    arriving_groups = in_phase.red_groups.intersection(out_phase.green_groups)
     return max(
-        group_intergreen_time(evacuating, incoming)
-        for (evacuating, arriving) in permutation(evacuating_groups, arriving_groups)
+        group_intergreen_time(evacuating, arriving)
+        for (evacuating, arriving) in product(evacuating_groups, arriving_groups)
     )
