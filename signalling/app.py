b7fd90deec2ec335440cbcf40374bc4baabf3ff1
Cielak <4882202+cielak@users.noreply.github.com>
2019-11-14 10:42:29 +0100

    Prepared data inputs for groups and streams

--- signalling/app.py
+++ signalling/app.py
@@ -11,12 +11,55 @@ app.layout = html.Div(
     [
         html.H2("Signalling"),
         dt.DataTable(
-            id="table",
-            columns=[{"name": i, "id": i} for i in df.columns],
-            data=df.to_dict("records"),
+            id="groups_table",
+            columns=[{"name": x, "id": x} for x in ["group_name"]],
+            data=[],
+            editable=True,
+            row_deletable=True,
         ),
+        html.Button("Add group", id="add_group_button", n_clicks=0),
+        dt.DataTable(
+            id="streams_table",
+            columns=[
+                {"name": x, "id": x}
+                for x in ["stream_name", "stream_group", "stream_yellow_time"]
+            ],
+            data=[],
+            editable=True,
+            row_deletable=True,
+        ),
+        html.Button("Add stream", id="add_stream_button", n_clicks=0),
     ]
 )
+
+
+@app.callback(
+    dash.dependencies.Output("groups_table", "data"),
+    [dash.dependencies.Input("add_group_button", "n_clicks")],
+    [
+        dash.dependencies.State("groups_table", "data"),
+        dash.dependencies.State("groups_table", "columns"),
+    ],
+)
+def add_group(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c["id"]: "" for c in columns})
+    return rows
+
+
+@app.callback(
+    dash.dependencies.Output("streams_table", "data"),
+    [dash.dependencies.Input("add_stream_button", "n_clicks")],
+    [
+        dash.dependencies.State("streams_table", "data"),
+        dash.dependencies.State("streams_table", "columns"),
+    ],
+)
+def add_stream(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c["id"]: "" for c in columns})
+    return rows
+
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8888)
