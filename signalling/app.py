d5d696e4fc763e1c1362661c8e5449e9a931b783
Cielak <4882202+cielak@users.noreply.github.com>
2019-11-20 20:19:16 +0100

    Multi selectable fields for streams in group

--- signalling/app.py
+++ signalling/app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_core_components as dcc
 import dash_table as dt
 import dash_html_components as html
 
@@ -37,16 +38,7 @@ app.layout = html.Div(
         ),
         html.H2("Signalling groups"),
         html.Button("Add group", id="add_group_button", n_clicks=0),
-        dt.DataTable(
-            id="groups_table",
-            columns=[
-                {"name": x, "id": x, "presentation": "dropdown"}
-                for x in SignallingGroup._fields
-            ],
-            data=[],
-            editable=True,
-            row_deletable=True,
-        ),
+        html.Div(id="groups_table_", children=[]),
     ]
 )
 
@@ -84,7 +76,7 @@ def add_collision_point(n_clicks, rows, columns):
     [dash.dependencies.Input("streams_table", "data_timestamp")],
     [dash.dependencies.State("streams_table", "data")],
 )
-def set_available_streams_for_collision_points(timestamp, rows):
+def set_available_streams_for_stream_intersections(timestamp, rows):
     vals = [x["stream_id"] for x in rows]
     options = {"options": [{"label": i, "value": i} for i in vals]}
     dropdown = {"arriving_stream": options, "evacuating_stream": options}
@@ -92,28 +84,28 @@ def set_available_streams_for_collision_points(timestamp, rows):
 
 
 @app.callback(
-    dash.dependencies.Output("groups_table", "data"),
+    dash.dependencies.Output("groups_table_", "children"),
     [dash.dependencies.Input("add_group_button", "n_clicks")],
     [
-        dash.dependencies.State("groups_table", "data"),
-        dash.dependencies.State("groups_table", "columns"),
+        dash.dependencies.State("groups_table_", "children"),
+        dash.dependencies.State("streams_table", "data"),
     ],
 )
-def add_group(n_clicks, rows, columns):
+def add_group(n_clicks, groups_rows, streams_rows):
     if n_clicks > 0:
-        rows.append({c["id"]: "" for c in columns})
-    return rows
-
-
-@app.callback(
-    dash.dependencies.Output("groups_table", "dropdown"),
-    [dash.dependencies.Input("streams_table", "data_timestamp")],
-    [dash.dependencies.State("streams_table", "data")],
-)
-def set_available_streams_for_groups(timestamp, rows):
-    vals = [x["stream_id"] for x in rows]
-    dropdown = {"streams": {"options": [{"label": i, "value": i} for i in vals]}}
-    return dropdown
+        vals = [x["stream_id"] for x in streams_rows]
+        options = [{"label": i, "value": i} for i in vals]
+        groups_rows.append(
+            html.Div(
+                children=[
+                    dcc.Input(id="group_id"),
+                    dcc.Dropdown(
+                        id="group_streams", options=options, value=[], multi=True,
+                    ),
+                ]
+            )
+        )
+    return groups_rows
 
 
 if __name__ == "__main__":
