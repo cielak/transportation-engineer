8f678154abd83028f471bd12616268f0c19be77b
Cielak <4882202+cielak@users.noreply.github.com>
2020-01-08 00:03:35 +0000

    Minor testing improvement

--- signalling/tests/test_signalling.py
+++ signalling/tests/test_signalling.py
@@ -1,12 +1,23 @@
 import pytest
 
 from signalling.logic import intergreen_time
+from signalling.models import TrafficStream, StreamIntersection
+
+
+class TestModels:
+    def test_intersection_inverts(self):
+        ts1 = TrafficStream("ts1", 1, 1, 1, 1)
+        ts1_dist = 1
+        ts2 = TrafficStream("ts2", 2, 2, 2, 2)
+        ts2_dist = 2
+        si = StreamIntersection(ts1, ts1_dist, ts2, ts2_dist)
+        assert si.inverted() == StreamIntersection(ts2, ts2_dist, ts1, ts1_dist)
 
 
 class TestLogic:
     @pytest.mark.parametrize(
         "evac_yellow_time, evac_time, arr_time, intergreen",
-        [[0, 0, 0, 0], [3, 10, 0, 13]],
+        [[0, 0, 0, 0], [3, 9.5, 0.1, 13], [0, 0, 12, 0]],
     )
     def test_intergreen_time(self, evac_yellow_time, evac_time, arr_time, intergreen):
         assert intergreen_time(evac_yellow_time, evac_time, arr_time) == intergreen
