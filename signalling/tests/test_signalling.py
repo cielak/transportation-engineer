22031db02eb37dd577dbbfb6bd12ce7dee18f052
Cielak <4882202+cielak@users.noreply.github.com>
2020-01-17 21:55:47 +0100

    Added place for group intergreen times testing

--- signalling/tests/test_signalling.py
+++ signalling/tests/test_signalling.py
@@ -1,6 +1,6 @@
 import pytest
 
-from signalling.logic import intergreen_time
+from signalling.logic import intergreen_time, groups_intergreen_times
 from signalling.models import TrafficStream, StreamIntersection
 
 
@@ -15,9 +15,21 @@ class TestModels:
 
 
 class TestLogic:
+    @pytest.fixture
+    def groups(self):
+        return set()
+
+    @pytest.fixture
+    def collisions(self):
+        return set()
+    
     @pytest.mark.parametrize(
         "evac_yellow_time, evac_time, arr_time, intergreen",
         [[0, 0, 0, 0], [3, 9.5, 0.1, 13], [0, 0, 12, 0]],
     )
     def test_intergreen_time(self, evac_yellow_time, evac_time, arr_time, intergreen):
         assert intergreen_time(evac_yellow_time, evac_time, arr_time) == intergreen
+
+    def test_groups_intergreen_times(self, groups, collisions):
+        intergreens = groups_intergreen_times(groups, collisions)
+        assert intergreens == []
