798c860c8840b46bdb85810d1fc5547f4fd49f7f
Cielak <4882202+cielak@users.noreply.github.com>
2020-02-07 01:05:51 +0000

    Updated stripes styling

--- stripes/render.py
+++ stripes/render.py
@@ -15,19 +15,57 @@ class SimpleRenderer:
 
 
 class SvgRenderer:
+    second_size = (5, 5)
+
     def render_second(self, second_type, insert=(0, 0)):
         gr = svgwrite.container.Group()
-        defaults = {"size": (5, 5), "insert": insert}
+        defaults = {"size": self.second_size, "insert": insert}
+        line_defaults = {"stroke": "black", "stroke_width": 0.5}
+        x, y = insert
+        h, w = self.second_size
         if second_type == SecondType.off:
             gr.add(svgwrite.shapes.Rect(**defaults, fill="gray"))
+            gr.add(
+                svgwrite.shapes.Line(**line_defaults, start=(x + w, y), end=(x, y + h))
+            )
+            gr.add(
+                svgwrite.shapes.Line(**line_defaults, start=(x, y), end=(x + w, y + h))
+            )
         elif second_type == SecondType.red:
             gr.add(svgwrite.shapes.Rect(**defaults, fill="red"))
+            gr.add(
+                svgwrite.shapes.Line(
+                    **line_defaults, start=(x, y + h / 2), end=(x + w, y + h / 2)
+                )
+            )
+        elif second_type == SecondType.red_yellow:
+            gr.add(svgwrite.shapes.Rect(insert=insert, size=(w, h / 2), fill="red"))
+            gr.add(
+                svgwrite.shapes.Rect(
+                    insert=(x, y + h / 2), size=(w, h / 2), fill="yellow"
+                )
+            )
+            gr.add(
+                svgwrite.shapes.Line(**line_defaults, start=(x + w, y), end=(x, y + h))
+            )
         elif second_type == SecondType.green:
             gr.add(svgwrite.shapes.Rect(**defaults, fill="green"))
         elif second_type == SecondType.yellow:
             gr.add(svgwrite.shapes.Rect(**defaults, fill="yellow"))
+        elif second_type == SecondType.green_blinking:
+            gr.add(svgwrite.shapes.Rect(size=(w / 2, h), insert=(x, y), fill="white"))
+            gr.add(
+                svgwrite.shapes.Rect(
+                    size=(w / 2, h), insert=(x + w / 2, y), fill="green"
+                )
+            )
+        elif second_type == SecondType.yellow_blinking:
+            gr.add(svgwrite.shapes.Rect(**defaults, fill="yellow"))
+            gr.add(
+                svgwrite.shapes.Line(**line_defaults, start=(x + w, y), end=(x, y + h))
+            )
         else:
-            raise ValueErrorr("Unknown signal second type")
+            raise ValueError("Unknown signal second type")
         return gr
 
     def render_group(self, group):
@@ -38,11 +76,14 @@ class SvgRenderer:
         for i, second in enumerate(group.seconds):
             x, y = (20 + 5 * i, 0)
             paragraph.add(self.render_second(second, insert=(x, y + 5)))
-            paragraph.add(dwg.line(stroke="black", start=(x, y + 10), end=(x, y + 5)))
+            tick_delta = 4 if i % 5 else 3
+            paragraph.add(
+                dwg.line(stroke="black", start=(x, y + tick_delta), end=(x, y + 5))
+            )
             if last_type != second:
                 paragraph.add(dwg.text(str(i), insert=(x, y + 3), font_size=4))
                 paragraph.add(
-                    dwg.line(stroke="black", start=(x, y + 4), end=(x, y + 5))
+                    dwg.line(stroke="black", start=(x, y + 10), end=(x, y + 5))
                 )
             last_type = second
         return paragraph
@@ -52,7 +93,9 @@ class SvgRenderer:
         for i in range(1 + max(len(g.seconds) for g in program.groups)):
             x = 25 + i * 5
             y = 10
-            dwg.add(dwg.line(stroke="black", start=(x, y), end=(x + 5, y)))
+            dwg.add(
+                dwg.line(stroke="black", stroke_width=0.5, start=(x, y), end=(x + 5, y))
+            )
             dwg.add(dwg.line(stroke="black", start=(x, y), end=(x, y + 3)))
             if i % 5 == 0:
                 dwg.add(dwg.line(stroke="black", start=(x, y), end=(x, y - 4)))
